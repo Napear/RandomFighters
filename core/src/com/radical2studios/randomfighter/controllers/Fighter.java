a7ad2f5bf17f197b991a1638a09a83e7bccfbfb3
Napear <napear@gmail.com>
2021-07-28 22:36:52 -0500

    Implimented Fighter class for animations

--- core/src/com/radical2studios/randomfighter/controllers/Fighter.java
+++ core/src/com/radical2studios/randomfighter/controllers/Fighter.java
@@ -1,6 +1,13 @@
 package com.radical2studios.randomfighter.controllers;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
@@ -12,16 +19,59 @@ public class Fighter extends Actor {
   private String name;
   private int health;
   private Rectangle hitbox;
+  private HashMap<FighterAnimationType, Animation<TextureRegion>> animations;
+  private FighterAnimationType activeAnimation = null;
+  private Animation<TextureRegion> animation;
+  private float elapsedTime, x, y;
+  private boolean isFlipped = false;
 
-  public Fighter(String name, TextureAtlas atlas){
+  public Fighter(String name, TextureAtlas atlas, float x, float y){
     this.atlas = atlas;
     this.name = name;
+    this.x = x;
+    this.y = y;
     health = 3;
     hitbox = new Rectangle();
+    animations = new HashMap<FighterAnimationType, Animation<TextureRegion>>();
+    storeAnimations();
+    setAnimation(FighterAnimationType.IDLE);
   }
 
-  public void setIdle(){
+  private void storeAnimations(){
+    Arrays.asList(FighterAnimationType.values()).forEach(key -> {
+      Animation<TextureRegion> animation = new Animation<TextureRegion>(1f/(16.5f + MathUtils.random(-0.2f, 0.2f)),
+                                                                        atlas.findRegions(key.toString())
+                                                                      );
+      animations.put(key, animation);
+    });
+  }
 
+  public void setAnimation (FighterAnimationType animationType){
+    if(activeAnimation == animationType) return;
+    activeAnimation = animationType;
+    animation = animations.get(animationType);
+    elapsedTime = 0;
+  }
+
+  @Override
+  public void draw(Batch batch, float parentAlpha) {
+    TextureRegion frame = animation.getKeyFrame(elapsedTime, isAlive());
+    if(isFlipped != frame.isFlipX()) frame.flip(true, false);
+    batch.draw(frame, x, y);
+  }
+
+  @Override
+  public void act(float delta) {    
+    super.act(delta);
+    elapsedTime += delta;
+  }
+
+  public void flip(){
+    isFlipped = !isFlipped;
+  }
+
+  public boolean isAlive(){
+    return health > 0;
   }
 
   public void attack(){
@@ -41,10 +91,6 @@ public class Fighter extends Actor {
   }
 
   public void takeHit(){
-    
-  }
-
-  public void die(){
-
+    health--; 
   }
 }
